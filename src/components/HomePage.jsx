767825d185504531bd6c0071d5ac240835190e36
abidformuli <abidformuli101@gmail.com>
2025-09-10 16:17:46 +0430

    Update something

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -87,7 +87,6 @@ export default function BottomNav() {
           ))}
         </ul>
       </nav>
-      <p>omid khan jan</p>
       {/* <p>abid</p> */}
       {/* Hero content */}
       <div className="   flex flex-col md:flex-row items-center justify-between gap-10 md:gap-20 py-12 ">
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -73,7 +73,7 @@ export default function Portfolio() {
       </div>
 
       {/* Portfolio Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {filteredProjects.map(project => (
           <div
             key={project.id}
